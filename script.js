5ef54cfe974ef9bd80046ca958376e203f661db1
Marco De Vito <marco.devito93@gmail.com>
2024-03-13 09:57:02 +0100

    modificato il programma ora, accetta e controlla il prompt dell'utente

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let numbersPrompt= prompt("dammi dei numeri divisi da virgola");
+// numbersPrompt= numbersPrompt.replace(/[^\d.,-]/g, '');
 let numbers= numbersPrompt.split(',')
+// numbers= numbers.filter(el => el!== "" ); 
+numbers= numbers.filter(el => !isNaN(el) ); 
 // let numbers = [3, 7, -2, 5, 8, 1, 2, 5, 6, -4];
+numbers= numbers.map(el => parseFloat(el));
+console.log(numbers);
 console.log(typeof numbers[0]);
 numbers.sort((a, b) => a - b);
 
